b8c6404476356e496a9628e658fcba3ecf73c35b
gemm123 <gemaakbar213@gmail.com>
2022-10-13 16:56:26 +0700

    fix: id model

--- models/category.js
+++ models/category.js
@@ -2,11 +2,6 @@ const { DataTypes } = require('sequelize');
 const db = require('../config/index');
 
 const Category = db.define('categories', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
   name: {
     type: DataTypes.STRING
   }
--- models/product.js
+++ models/product.js
@@ -3,11 +3,6 @@ const db = require('../config/index');
 const Category = require('./category')
 
 const Product = db.define('products', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
   name: {
     type: DataTypes.STRING
   },
