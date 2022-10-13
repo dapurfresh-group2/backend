7f6cbe883ff41a100624f5c1ad029d6a9de15c57
Surya <febriantosurya@ce.student.pens.ac.id>
2022-10-14 00:19:38 +0700

    feat: add search product in controller, endpoint in routes, and request.rest in gitignore

--- controllers/product.js
+++ controllers/product.js
@@ -9,3 +9,19 @@ exports.getAllProduct = async (req, res) => {
         res.status(400).json({ message: `failed ${error.message}` })
     }
 }
+
+exports.searchProduct = async (req, res) => {
+    try {
+        const name_input = req.params.name
+        let filtered_products = []
+        const products = await productRepository.getAllProduct();
+        for (let i = 0; i < products.length; i++) {
+            if (((products[i].name).toLowerCase()).includes(name_input.toLowerCase())) {
+                filtered_products.push(products[i])
+            }
+        }
+        res.status(200).json({ message: "success", data: filtered_products });
+    } catch (error) {
+        res.status(404).json({ message: "product not found" });
+    };
+};
--- routes/product.js
+++ routes/product.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const productController = require('../controllers/product')
 
 router.get('/', productController.getAllProduct)
+router.get('/:name', productController.searchProduct)
 
 module.exports = router
