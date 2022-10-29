c3aded184ac56fe556e3ac68e742fcef1246082a
Surya <febriantosurya@ce.student.pens.ac.id>
2022-10-29 18:16:05 +0700

    fix: upload image in updateProfile function

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require("bcrypt");
 const userRepository = require('../repository/user')
 const profileRepository = require('../repository/profile')
+
 require('dotenv').config()
 
 exports.getProfile = async (req, res) => {
@@ -26,13 +27,19 @@ exports.updateProfile = async (req, res) => {
             res.status(400).json({ message: "username not registered" })
             return
         }
-        const {name, phone, address, image} = req.body
-        const updatedProfile = await profileRepository.updateUser(dataId.id, name, phone, address, image)
-        
-        res.status(200).json({ message: "success",data: updatedProfile })
+        const {name, phone, address} = req.body
+        if (!req.files) {
+            const updatedProfile = await profileRepository.updateUser(dataId.id, name, phone, address)
+            res.status(200).json({ message: "success",data: updatedProfile })
+        } 
+        else {
+            const file = req.files.image
+            const path = `/public/images/profile/${req.user.username}.jpg`
+            file.mv("." + path)
+            const updatedProfile = await profileRepository.updateUser(dataId.id, name, phone, address, path)
+            res.status(200).json({ message: "success",data: updatedProfile })
+        }
     } catch (error) {
         res.status(400).json({ message: `failed ${error.message}` })
     }
 }
-
-
