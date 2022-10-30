46133ab46a3f8f40ddd31064b49fd2b375822ac8
gemm123 <gemaakbar213@gmail.com>
2022-10-30 15:08:26 +0700

    feat: getcart endpoint

--- routes/cart.js
+++ routes/cart.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const cartController = require('../controllers/cart')
 const middleware = require('../middleware')
 
+router.get('/', middleware.checkAuthorization, cartController.getCartActive)
 router.post('/post-product/:id', middleware.checkAuthorization, cartController.postProduct)
 
 module.exports = router
